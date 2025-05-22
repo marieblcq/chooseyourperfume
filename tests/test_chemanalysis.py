fad7462e1d44f2d4c231559002d7570b2b2580a4
vicollari <victoire.ollari@apfl.ch>
2025-05-22 18:42:20 +0200

    removed unnecessary imports

--- tests/test_chemanalysis.py
+++ tests/test_chemanalysis.py
@@ -1,10 +1,6 @@
-import os
-import sys
 import numpy as np
 from rdkit import Chem
 from rdkit.Chem import DataStructs
-
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from chooseyourperfume.chem_analysis import (
     canonicalize_smiles,
@@ -13,8 +9,6 @@ from chooseyourperfume.chem_analysis import (
     scent_categories,
     run_pipeline
 )
-
-import subprocess
 
 # Test 1: SMILES Canonicalization Valid Case
 def test_canonicalize_smiles_valid():
